b5ea931b1d3845c87999bae6addd06fb13870eab
Diego Paz <dpaz52418@gmail.com>
2026-01-19 20:43:50 -0800

    Implemented Backgrounds + Starting Scripting

---
+++ Assets/Scripts/VectorGenerator.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VectorGenerator : MonoBehaviour
+{
+    public Color[] colors; // indexes 0-9
+    public Sprite[] fronts; // indexes 0-4
+    public Sprite[] backs; // indexes 0-4
+    public Sprite[] middles; // indexes 0-4
+
+    public GameObject frameObject; // the "Frame" object to turn on
+    public SpriteRenderer backgroundRenderer; // background color
+    public SpriteRenderer[] children; // back, middle, front
+    
+    Color selectedColor; // used to tint the background and layers
+    Sprite selectedFront;
+    Sprite selectedMiddle;
+    Sprite selectedBack;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Turn on "Frame" object
+        //frameObject.SetActive(true);
+        
+
+
+        /*
+        selectedColor = colors[Random.Range(0, colors.Length)];
+        selectedFront = fronts[Random.Range(0, fronts.Length)];
+        selectedMiddle = middles[Random.Range(0, middles.Length)];
+        selectedBack = backs[Random.Range(0, backs.Length)];
+        */
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AwesomeButton()
+    {
+        frameObject.SetActive(true);
+        Debug.Log("Button Pressed");
+    }
+}
